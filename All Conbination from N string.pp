af463044eac79842bd3480cc82b092889dce7f70
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2022-01-21 18:26:54 +0600

    Create All Conbination from N string.pp

---
+++ All Conbination from N string.pp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    vector<char>combination;
+    vector<string>ansCombination;
+    
+    void allCombination(vector<string>vec,int indx){
+        
+        if(indx>=vec.size()){
+            string str;
+            for(int i=0;i<combination.size();i++){
+                str+=combination[i];
+                cout<<combination[i];
+            }
+            ansCombination.push_back(str);
+            cout<<endl;
+            return;
+        }
+        for(int i=0;i<vec[indx].size();i++){
+            
+            combination.push_back(vec[indx][i]);
+            allCombination(vec,indx+1);
+            combination.pop_back();
+        }
+    }
+};
